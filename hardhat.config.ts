dfb58280ef8fff6a37be847e18fd0b87063939a7
nauh_pv <vanhuana4k56@gmail.com>
2025-03-04 13:19:01 +0700

    add config netword in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+require("dotenv").config();
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    zenc_testnet: {
+      url: "https://zenchain-testnet.api.onfinality.io/public",
+      chainId: 8408,
+      accounts: [process.env.PRIVATE_KEY1 as string],
+      gas: 5000000,
+      gasPrice: 5000000000,
+    },
+  },
 };
 
 export default config;
